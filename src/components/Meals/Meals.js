56288cfdfb9e3fb4f264a1689a91239ce98b57e8
Deeksha Jagtap <67088622+deek121477@users.noreply.github.com>
2023-08-05 14:53:23 +0530

    Add files via upload

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import MealsSummary from "./MelasSummary";
+import AvailableMeals from "./AvailableMeals";
+
+const Meals=()=>{
+return <Fragment>
+    <MealsSummary/>
+    <AvailableMeals/>
+</Fragment>
+}
+
+export default Meals;
